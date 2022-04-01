71b50f49172e32d591bd666e4f6e3e1d32812674
KDNKrishna <kdnkrishna@gmail.com>
2022-04-01 20:35:42 +0200

    Add files via upload

---
+++ kitchen_calculator.ex
@@ -0,0 +1,41 @@
+defmodule KitchenCalculator do
+  def get_volume(volume_pair) do
+    elem(volume_pair, 1)
+  end
+  def to_milliliter({:cup, volume}) do
+    {:milliliter, volume * 240}
+  end
+  def to_milliliter({:fluid_ounce, volume}) do
+    {:milliliter, volume * 30}
+  end
+  def to_milliliter({:teaspoon, volume}) do
+    {:milliliter, volume * 5}
+  end
+  def to_milliliter({:tablespoon, volume}) do
+    {:milliliter, volume * 15}
+  end
+  def to_milliliter({:milliliter, volume}) do
+    {:milliliter, volume}
+  end
+  def from_milliliter(volume_pair, :cup) do
+    {:cup, get_volume(volume_pair) / 240}
+  end
+  def from_milliliter(volume_pair, :fluid_ounce) do
+    {:fluid_ounce, get_volume(volume_pair) / 30}
+  end
+  def from_milliliter(volume_pair, :teaspoon) do
+    {:teaspoon, get_volume(volume_pair) / 5}
+  end
+  def from_milliliter(volume_pair, :tablespoon) do
+    {:tablespoon, get_volume(volume_pair) / 15}
+  end
+  def from_milliliter(volume_pair, :milliliter) do
+    volume_pair
+  end
+  def convert({unit, volume}, requested_unit) when unit == requested_unit do
+    {unit, volume}
+  end
+  def convert(volume_pair, unit) do
+    from_milliliter(to_milliliter(volume_pair), unit)
+  end
+end
