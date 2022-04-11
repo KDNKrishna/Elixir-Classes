efc436d5c2b4cf423bff7bc572b5d461ff0e9959
KDNKrishna <kdnkrishna@gmail.com>
2022-04-11 12:17:44 +0200

    Add files via upload

---
+++ nameBadgeIf.ex
@@ -0,0 +1,27 @@
+defmodule NameBadge do
+  @moduledoc """
+    Print name badges for factory employees. Employees have an ID, name, and department name. Employee badge labels are formatted as "[id] - name - DEPARTMENT".
+  """
+
+  @doc """
+  When the department is missing, assume the badge belongs to the company owner.
+  """
+  @spec print(non_neg_integer(), String.t(), String.t()) :: String.t()
+  def print(id, name, nil) do
+    print(id, name, "OWNER")
+  end
+
+
+  @doc """
+  It should take an id, name, and a department. It should return the badge label, with the department name in uppercase. Also when the id is missing, it should print a badge without it.
+  """
+  @spec print(non_neg_integer(), String.t(), String.t()) :: String.t()
+  def print(id, name, department) do
+    # Please implement the print/3 function
+    if id != nil do
+      "[#{id}]" <> " - " <> name <> " - " <> String.upcase(department)
+    else
+      name <> " - " <> String.upcase(department)
+    end
+  end
+end
