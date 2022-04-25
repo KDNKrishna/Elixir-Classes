027195b50d9cb92514196c0f979f9f0ee2165c6b
KDNKrishna <kdnkrishna@gmail.com>
2022-04-25 20:51:20 +0200

    Update birdCountRecursion.ex

--- birdCountRecursion.ex
+++ birdCountRecursion.ex
@@ -7,54 +7,51 @@ defmodule BirdCount do
    This function should take a list of daily bird counts and return today's count. If the list is empty, it should return nil
   """
   @spec today(List) :: Integer
-  def today(list) do
-    # Please implement the today/1 function
-    List.first(list)
-  end
+  def today([]), do: nil
+  def today([h | _t]), do: h
 
   @doc """
    If the list is empty, return [1]
   """
   @spec increment_day_count(List) :: List
-  def increment_day_count([]) do
-    [1]
-  end
+  def increment_day_count([]), do: [1]  
 
-  @doc """
+    @doc """
    This function should take a list of daily bird counts and increment the today's count by 1.
   """
   @spec increment_day_count(List) :: List
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-    1+hd(list)
-  end
+  def increment_day_count([h | t]), do: [h + 1 | t]
 
   @doc """
   It should take a list of daily bird counts. It should return true if there was at least one day when no birds visited the garden, and false otherwise.
   """
   @spec has_day_without_birds?(List) :: boolean()
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-    0 in list
-  end
+  def has_day_without_birds?([]), do: false
+
+  @doc """
+  It should take a list of daily bird counts. It should return true if there was at least one day when no birds visited the garden, and false otherwise.
+  """
+  @spec has_day_without_birds?(List) :: boolean()
+  def has_day_without_birds?([h | t]), do: h == 0 or has_day_without_birds?(t)
 
   @doc """
   It should take a list of daily bird counts and return the total number that visited your garden
   """
   @spec total(List) :: Integer
-  def total(list) do
-    # Please implement the total/1 function
-    Enum.sum(list)
-  end
+  def total([]), do: 0
+
+  @doc """
+  It should take a list of daily bird counts and return the total number that visited your garden
+  """
+  @spec total(List) :: Integer
+  def total([h | t]), do: h + total(t)
 
   @doc """
    It should take a list of daily bird counts and return the number of busy days.
    A busy day is one where five or more birds have visited your garden.
   """
   @spec busy_days(List) :: Integer
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-    a = Enum.filter(list, fn x -> (x == 5 or x > 5) end)
-    length(a)
-  end
+  def busy_days([]), do: 0
+  def busy_days([h | t]) when h >= 5, do: 1 + busy_days(t)
+  def busy_days([h | t]), do: busy_days(t)
 end
